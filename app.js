661df27859e757a666a657c5a2254ab62f23b11f
guillaume-braibant <guillaume.braibant@nrb.be>
2020-02-11 19:55:52 +0100

    add HOST variable + remaster swagger

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes/Routes');
-const port = process.env.PORT;
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || "localhost";
+const base = "/api/v1";
 
 // Setup apps
 const app = express();
@@ -10,8 +14,49 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Swagger set up
+const options = {
+    swaggerDefinition: {
+      openapi: "3.0.0",
+      info: {
+        title: "Say Hello API",
+        version: "1.0.0",
+        description:
+          "A simple Say Hello API",
+        license: {
+          name: "Apache-2.0",
+          url: "https://choosealicense.com/licenses/apache-2.0/"
+        },
+        contact: {
+          name: "Swagger",
+          url: "https://swagger.io",
+          email: "Info@SmartBear.com"
+        }
+      },
+      servers: [
+        {
+          url: "http://" + host + ":" + port + base
+        }
+      ]
+    },
+    apis: [
+      "./models/person.js",
+      "./models/greeting.js",
+      "./routes/routes.js"
+    ]
+  };
+  
+  const specs = swaggerJsdoc(options);
+  app.use(base + "/docs", swaggerUi.serve);
+  app.get(
+    base + "/docs",
+    swaggerUi.setup(specs, {
+      explorer: true
+    })
+  );
+
 // Include routes
-app.use("/api/v1", routes);
+app.use(base, routes);
 
 // Start the app
-app.listen(port, () => console.log(`App listening on port ${port}!`));
+app.listen(port, () => console.log(`App listening on host ${host} on port ${port}!`));
--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const uuid = require('uuid-random');
-const swaggerJsdoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
 const router = express.Router();
 
 const Person = require('../models/Person');
@@ -43,47 +41,6 @@ router.post('/hello', (req, res) => {
     var greeting = new Greeting(person.firstName, person.lastName, app_uuid)
     res.json(greeting) 
 });
-
-// Swagger set up
-const options = {
-  swaggerDefinition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Say Hello API",
-      version: "1.0.0",
-      description:
-        "A simple Say Hello API",
-      license: {
-        name: "Apache-2.0",
-        url: "https://choosealicense.com/licenses/apache-2.0/"
-      },
-      contact: {
-        name: "Swagger",
-        url: "https://swagger.io",
-        email: "Info@SmartBear.com"
-      }
-    },
-    servers: [
-      {
-        url: "http://localhost:3000/api/v1"
-      }
-    ]
-  },
-  apis: [
-    "./models/person.js",
-    "./models/greeting.js",
-    "./routes/routes.js"
-  ]
-};
-
-const specs = swaggerJsdoc(options);
-router.use("/docs", swaggerUi.serve);
-router.get(
-  "/docs",
-  swaggerUi.setup(specs, {
-    explorer: true
-  })
-);
 
 // Catch 404 and forward to error handler
 router.use(function(req, res, next) {
